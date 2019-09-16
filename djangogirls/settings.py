4c62aae816b2ab6f69aef32faf23654a5712d0d9
Nelzio <nelziositoe@gmail.com>
2019-09-16 20:01:32 +0200

    Blog Migration

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig' # can be 'blog' only, but i like 'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
