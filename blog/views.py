5915e508a52d1ad6e64e887dc51d3192947ac793
Nelzio <nelziositoe@gmail.com>
2019-09-16 20:27:20 +0200

    Template e css

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def blog(request):
     data = {}
+    data['posts'] = Post.objects.all()
     return render(request, 'blog/index.html', data)
